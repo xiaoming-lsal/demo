e635df0f9849f6948208fcc8c9f61659073d3432
xiaoming <1070659975@qq.com>
2019-11-29 20:54:16 +0800

    XiaoMing

--- src/main/java/cn/lsal/App.java
+++ src/main/java/cn/lsal/App.java
@@ -10,5 +10,6 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println("Hello World!");
         System.out.println("Hello Git!");
+        System.out.println("Hello XiaoMing!");
     }
 }
