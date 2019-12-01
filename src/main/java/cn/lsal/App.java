8f5e0c77132d4e0c5dfb2b8f53988b19fa602401
xiaoming <1070659975@qq.com>
2019-12-01 12:17:21 +0800

    Many Process

--- src/main/java/cn/lsal/App.java
+++ src/main/java/cn/lsal/App.java
@@ -1,15 +1,23 @@
 package cn.lsal;
 
+import cn.lsal.mydemo.Baskatball;
+
+import java.util.Date;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args ) {
+    public static void main(String[] args) throws InterruptedException {//中断的，打断的Interrupted
         System.out.println( "Hello World!" );
         System.out.println("Hello World!");
         System.out.println("Hello Git!");
         System.out.println("Hello XiaoMing!");
+        Baskatball baskatball = new Baskatball();
+        baskatball.wait();
+        Date date = new Date();
+        System.out.println(date + "学会多线程！");
     }
 }
---
+++ src/main/java/cn/lsal/mydemo/Baskatball.java
@@ -0,0 +1,13 @@
+package cn.lsal.mydemo;
+
+public class Baskatball {
+    private String name;
+
+    public Baskatball(String name) {
+        this.name = name;
+    }
+
+    public Baskatball() {
+
+    }
+}
